20f3b64ec8f4139c10fd97d5f298721ea8a8a75e
Nicholas Lemay <lemaynicholas@gmail.com>
2023-12-02 16:52:51 -0500

    Solves part2

--- Day01/Spec/part2_spec.rb
+++ Day01/Spec/part2_spec.rb
@@ -2,7 +2,8 @@
 class Calibration
 
   def self.calibration_of_instruction(instruction)
-    "#{self.first_digit_of(instruction).to_i}#{self.last_digit_of(instruction)}".to_i
+    sanitized_instruction = self.sanitize(instruction)
+    "#{self.first_digit_of(sanitized_instruction).to_i}#{self.last_digit_of(sanitized_instruction)}".to_i
   end
 
   def self.first_digit_of(instruction)
@@ -15,11 +16,31 @@ class Calibration
 
   private
 
+  def self.sanitize(instruction)
+    sanitized_string = instruction
+    {
+      "one" => "o1e",
+      "two" => "t2o",
+      "three" => "t3e",
+      "four" => "f4r",
+      "five" => "f5e",
+      "six" => "s6x",
+      "seven" => "s7n",
+      "eight" => "e8t",
+      "nine" => "n9e",
+    }.each{|k,v|
+      sanitized_string.gsub!( k, v)
+    }
+    sanitized_string
+  end
+
+
   def self.get_match_from_regex_or_empty(string, regexp)
     match = string.match(regexp)
 
     match.nil? ? '' : match[1]
   end
+
 
 end
 
@@ -47,6 +68,20 @@ RSpec.describe "Calibration" do
   it 'solves provided sample' do
     sample_result = %w(1abc2 pqr3stu8vwx a1b2c3d4e5f treb7uchet).sum(0){|i| Calibration.calibration_of_instruction(i)}
     expect(sample_result).to eq(142)
+  end
+
+  it 'handles instruction containing numbers as string' do
+    expect(Calibration.calibration_of_instruction('two1nine')).to eq(29)
+    expect(Calibration.calibration_of_instruction('eightwothree')).to eq(83)
+    expect(Calibration.calibration_of_instruction('abcone2threexyz')).to eq(13)
+    expect(Calibration.calibration_of_instruction('4nineeightseven2')).to eq(42)
+    expect(Calibration.calibration_of_instruction('zoneight234')).to eq(14)
+    expect(Calibration.calibration_of_instruction('7pqrstsixteen')).to eq(76)
+  end
+
+  it 'solves my own input data' do
+    result = File.readlines('Day01/Spec/day_01_input.txt', chomp:true).sum(0){|i| Calibration.calibration_of_instruction(i)}
+    expect(result).to eq(53921)
   end
 
 end
