63f29b458a357f43eb278603476b54652d7e34c7
Nicholas Lemay <lemaynicholas@gmail.com>
2023-12-02 15:29:00 -0500

    Calibration returns last digit of empty string

--- Day01/Spec/part1_spec.rb
+++ Day01/Spec/part1_spec.rb
@@ -6,6 +6,10 @@ class Calibration
 
     match.nil? ? '' : match[1]
   end
+
+  def self.last_digit_of(instruction)
+    ''
+  end
 end
 
 RSpec.describe "Calibration" do
@@ -14,6 +18,10 @@ RSpec.describe "Calibration" do
     expect(Calibration.first_digit_of('')).to eq('')
     expect(Calibration.first_digit_of('1abc2')).to eq('1')
     expect(Calibration.first_digit_of('pqr3stu8vwx')).to eq('3')
+  end
+
+  it 'return last digit of calibration value' do
+    expect(Calibration.last_digit_of('')).to eq('')
   end
 end
 
