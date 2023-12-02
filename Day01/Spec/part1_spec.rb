7aceacccf4f1efd4fd19ba66d05257f55c430617
Nicholas Lemay <lemaynicholas@gmail.com>
2023-12-02 15:27:41 -0500

    Calibration returns first digit of string containing many digits

--- Day01/Spec/part1_spec.rb
+++ Day01/Spec/part1_spec.rb
@@ -4,7 +4,7 @@ class Calibration
   def self.first_digit_of(instruction)
     match = instruction.match(/^\D*(\d+)/)
 
-    match.nil? ? '' : match[0]
+    match.nil? ? '' : match[1]
   end
 end
 
@@ -13,6 +13,7 @@ RSpec.describe "Calibration" do
   it 'return first digit of calibration value' do
     expect(Calibration.first_digit_of('')).to eq('')
     expect(Calibration.first_digit_of('1abc2')).to eq('1')
+    expect(Calibration.first_digit_of('pqr3stu8vwx')).to eq('3')
   end
 end
 
