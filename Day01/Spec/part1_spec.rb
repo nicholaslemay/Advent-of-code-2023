de0ff249c4c04f580dbcf7985d79a6b1a991f006
Nicholas Lemay <lemaynicholas@gmail.com>
2023-12-02 23:00:16 -0500

    Part1 is more compact

--- Day01/Spec/part1_spec.rb
+++ Day01/Spec/part1_spec.rb
@@ -2,23 +2,11 @@
 class Calibration
 
   def self.calibration_of_instruction(instruction)
-    "#{self.first_digit_of(instruction).to_i}#{self.last_digit_of(instruction)}".to_i
+    "#{self.digit_at_position(instruction, :first)}#{self.digit_at_position(instruction, :last)}".to_i
   end
 
-  def self.first_digit_of(instruction)
-    self.get_match_from_regex_or_empty(instruction, /^\D*(\d)/)
-  end
-
-  def self.last_digit_of(instruction)
-    self.get_match_from_regex_or_empty(instruction, /(\d)\D*\Z/)
-  end
-
-  private
-
-  def self.get_match_from_regex_or_empty(string, regexp)
-    match = string.match(regexp)
-
-    match.nil? ? '' : match[1]
+  def self.digit_at_position(instruction, position)
+    instruction.scan(/\d/).send(position) || ''
   end
 
 end
@@ -26,18 +14,18 @@ end
 RSpec.describe "Calibration" do
 
   it 'return first digit of calibration value' do
-    expect(Calibration.first_digit_of('')).to eq('')
-    expect(Calibration.first_digit_of('1abc2')).to eq('1')
-    expect(Calibration.first_digit_of('pqr3stu8vwx')).to eq('3')
-    expect(Calibration.first_digit_of('eight47vrvjlpgcqthree87q')).to eq('4')
+    expect(Calibration.digit_at_position('', :first)).to eq('')
+    expect(Calibration.digit_at_position('1abc2', :first)).to eq('1')
+    expect(Calibration.digit_at_position('pqr3stu8vwx', :first)).to eq('3')
+    expect(Calibration.digit_at_position('eight47vrvjlpgcqthree87q', :first)).to eq('4')
 
   end
 
   it 'return last digit of calibration value' do
-    expect(Calibration.last_digit_of('')).to eq('')
-    expect(Calibration.last_digit_of('treb7uchet')).to eq('7')
-    expect(Calibration.last_digit_of('1abc2')).to eq('2')
-    expect(Calibration.last_digit_of('eight47vrvjlpgcqthree39q')).to eq('9')
+    expect(Calibration.digit_at_position('', :last)).to eq('')
+    expect(Calibration.digit_at_position('treb7uchet', :last)).to eq('7')
+    expect(Calibration.digit_at_position('1abc2', :last)).to eq('2')
+    expect(Calibration.digit_at_position('eight47vrvjlpgcqthree39q', :last)).to eq('9')
   end
 
   it 'returns calibration of single instruction' do
