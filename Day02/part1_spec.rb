a242bbaf45d05901fc721ffa31ffac0afb91a257
Nicholas Lemay <lemaynicholas@gmail.com>
2023-12-03 21:58:34 -0500

    Game get multiple digit id built from instructions

--- Day02/part1_spec.rb
+++ Day02/part1_spec.rb
@@ -4,7 +4,7 @@ class Game
 
   def self.built_from(instruction)
     game = Game.new
-    game.id = instruction.scan(/Game (\d)/)[0][0]
+    game.id = instruction.scan(/Game (\d+)/)[0][0]
     game
   end
 
@@ -15,6 +15,7 @@ RSpec.describe "Game" do
   describe 'built from record' do
     it 'has an id' do
       expect(Game.built_from('Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green').id).to eq('1')
+      expect(Game.built_from('Game 12: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green').id).to eq('12')
     end
   end
 
