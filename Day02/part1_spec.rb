7b5bec5b224970385f9d08c63f8873abbd633196
Nicholas Lemay <lemaynicholas@gmail.com>
2023-12-03 21:58:06 -0500

    Game get single digit id buil;t from instructions

--- Day02/part1_spec.rb
+++ Day02/part1_spec.rb
@@ -4,10 +4,9 @@ class Game
 
   def self.built_from(instruction)
     game = Game.new
-    game.id = ''
+    game.id = instruction.scan(/Game (\d)/)[0][0]
     game
   end
-
 
 end
 
@@ -15,7 +14,7 @@ RSpec.describe "Game" do
 
   describe 'built from record' do
     it 'has an id' do
-      expect(Game.built_from('Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green').id).to eq('')
+      expect(Game.built_from('Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green').id).to eq('1')
     end
   end
 
