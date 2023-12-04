b390f1cc7a2edd1402e82dad1e3999776e68b6eb
Nicholas Lemay <lemaynicholas@gmail.com>
2023-12-03 22:42:29 -0500

    Removing builder

--- Day02/part1_spec.rb
+++ Day02/part1_spec.rb
@@ -9,16 +9,10 @@ class GameSet
   def self.built_from(set_instruction)
     set = GameSet.new
 
-    if(set_instruction =~ /(\d+) blue/)
-      set.blue = $1.to_i
-    end
-
-    if(set_instruction =~ /(\d+) red/)
-      set.red = $1.to_i
-    end
-
-    if(set_instruction =~ /(\d+) green/)
-      set.green = $1.to_i
+    %w(red green blue).each do |color|
+      if(set_instruction =~ /(\d+) #{color}/)
+        set.send("#{color}=", $1.to_i)
+      end
     end
 
     set
@@ -29,21 +23,16 @@ class Game
   attr_accessor :id
   attr_accessor :sets
 
-  def initialize
+  def initialize(instruction)
     @sets = []
+    @id = instruction.scan(/Game (\d+)/)[0][0]
+    instruction.split(';').each do |part|
+      @sets << GameSet.built_from(part)
+    end
   end
 
   def is_possible?(treshold)
     sets.all?{|s| s.red <= treshold[:red] && s.blue <= treshold[:blue] && s.green <= treshold[:green] }
-  end
-
-  def self.built_from(instruction)
-    game = Game.new
-    game.id = instruction.scan(/Game (\d+)/)[0][0]
-    instruction.split(';').each do |part|
-      game.sets << GameSet.built_from(part)
-    end
-    game
   end
 
 end
@@ -52,18 +41,17 @@ RSpec.describe "Game" do
 
   describe 'built from record' do
     it 'has an id' do
-      expect(Game.built_from('Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green').id).to eq('1')
-      expect(Game.built_from('Game 12: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green').id).to eq('12')
+      expect(Game.new('Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green').id).to eq('1')
+      expect(Game.new('Game 12: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green').id).to eq('12')
     end
 
     it 'has number or sets equal to number seperated by ; in instructions' do
-      expect(Game.built_from('Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green').sets.length).to eq(3)
-      expect(Game.built_from('Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green').sets.length).to eq(2)
+      expect(Game.new('Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green').sets.length).to eq(3)
+      expect(Game.new('Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green').sets.length).to eq(2)
     end
 
-
     it 'has number or sets equal to number seperated by ; in instructions' do
-      game = Game.built_from('Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green')
+      game = Game.new('Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green')
       expect(game.sets[0].blue).to eq(3)
       expect(game.sets[0].red).to eq(4)
       expect(game.sets[0].green).to eq(0)
@@ -72,19 +60,18 @@ RSpec.describe "Game" do
 
     it 'can say whether a game is valid or not' do
       treshold = {:red=> 12, :green => 13, :blue => 14}
-      expect(Game.built_from('Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green').is_possible?(treshold)).to eq(true)
-      expect(Game.built_from('Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue').is_possible?(treshold)).to eq(true)
-      expect(Game.built_from('Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red').is_possible?(treshold)).to eq(false)
-      expect(Game.built_from('Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red').is_possible?(treshold)).to eq(false)
-      expect(Game.built_from('Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green').is_possible?(treshold)).to eq(true)
+      expect(Game.new('Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green').is_possible?(treshold)).to eq(true)
+      expect(Game.new('Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue').is_possible?(treshold)).to eq(true)
+      expect(Game.new('Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red').is_possible?(treshold)).to eq(false)
+      expect(Game.new('Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red').is_possible?(treshold)).to eq(false)
+      expect(Game.new('Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green').is_possible?(treshold)).to eq(true)
     end
-
 
     it 'can resolve my riddle' do
       treshold = {:red=> 12, :green => 13, :blue => 14}
 
       result = File.readlines('Day02/day_02_input.txt', chomp:true).sum(0) do |i|
-           game = Game.built_from(i)
+           game = Game.new(i)
            game.is_possible?(treshold) ? game.id.to_i : 0
         end
       expect(result).to eq(2239)
