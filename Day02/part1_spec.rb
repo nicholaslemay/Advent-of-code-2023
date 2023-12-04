1d56ec2da7f5acd0fedef0f76cabefff4ba97486
Nicholas Lemay <lemaynicholas@gmail.com>
2023-12-03 22:06:16 -0500

    Game has variable amount of sets based on number of entries in instructions

--- Day02/part1_spec.rb
+++ Day02/part1_spec.rb
@@ -1,10 +1,18 @@
 
 class Game
   attr_accessor :id
+  attr_accessor :sets
+
+  def initialize
+    @sets = []
+  end
 
   def self.built_from(instruction)
     game = Game.new
     game.id = instruction.scan(/Game (\d+)/)[0][0]
+    instruction.split(';').each do |part|
+      game.sets << ''
+    end
     game
   end
 
@@ -16,6 +24,11 @@ RSpec.describe "Game" do
     it 'has an id' do
       expect(Game.built_from('Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green').id).to eq('1')
       expect(Game.built_from('Game 12: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green').id).to eq('12')
+    end
+
+    it 'has number or sets equal to number seperated by ; in instructions' do
+      expect(Game.built_from('Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green').sets.length).to eq(3)
+      expect(Game.built_from('Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green').sets.length).to eq(2)
     end
   end
 
